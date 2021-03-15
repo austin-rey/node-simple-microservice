6a07d9408009d4ae6db435455b446aff55f589c9
austin-rey <austinrey95@gmail.com>
2021-03-15 13:19:28 -0500

    Introducent moderation service across app

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,9 +9,10 @@ app.use(bodyParser.json())
 app.post('/events', (req,res) => {
     const event = req.body;
 
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
+    axios.post('http://localhost:4000/events', event); // Post
+    axios.post('http://localhost:4001/events', event); // Comment
+    axios.post('http://localhost:4002/events', event); // Query
+    axios.post('http://localhost:4003/events', event); // Moderation
 
     res.send({status: 'Ok'})
 })
